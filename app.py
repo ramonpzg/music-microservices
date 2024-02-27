15cc5f6c6a63a35b0b036f85a9e30560237dc6fc
Ramon Perez <ramonpzg@protonmail.com>
2024-02-27 12:21:33 +0000

    restructured repo

--- app.py
+++ app.py
@@ -1,6 +1,98 @@
-from src.plotting import make_waveform, make_spectogram
-from src.helpers import *
 import gradio as gr
+from pedalboard.io import AudioFile
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import numpy as np
+
+from faker import Faker
+import requests
+
+from glob import glob
+import os
+
+
+def get_latest_file():
+    files = glob("./music/*.mp3")
+    latest_file = max(files, key=os.path.getctime)
+    with AudioFile(latest_file, "r") as f:
+        waveform = f.read(f.frames)
+        sample_rate = f.samplerate
+    return waveform, sample_rate
+
+
+def make_waveform():
+    waveform, sample_rate = get_latest_file()
+
+    num_channels, num_frames = waveform.shape
+    time_axis = np.arange(0, num_frames) / sample_rate
+    with plt.xkcd():
+        figure = Figure()
+        axes = figure.subplots(num_channels, 1)
+        if num_channels == 1:
+            axes = [axes]
+        for c in range(num_channels):
+            axes[c].plot(time_axis, waveform[c], linewidth=1)
+            axes[c].grid(True)
+            if num_channels > 1:
+                axes[c].set_ylabel(f"Channel {c+1}")
+        figure.suptitle("Waveform")
+    return figure
+
+
+def make_spectogram():
+    waveform, sample_rate = get_latest_file()
+
+    num_channels, num_frames = waveform.shape
+    with plt.xkcd():
+        figure = Figure()
+        axes = figure.subplots(num_channels, 1)
+        if num_channels == 1:
+            axes = [axes]
+        for c in range(num_channels):
+            axes[c].specgram(waveform[c], Fs=sample_rate)
+            if num_channels > 1:
+                axes[c].set_ylabel(f"Channel {c+1}")
+        figure.suptitle("Spectrogram")
+    return figure
+
+
+def make_sound(text, guidance_scale, max_new_tokens, sample_rate):
+    faker = Faker()
+    # endpoint = "http://localhost:8080/v2/models/musicgen_model/infer"
+    # input_request = {
+    #     'inputs': [
+    #         StringCodec.encode_input(name='text', payload=[text], use_bytes=False).dict(),
+    #         NumpyCodec.encode_input(name='guidance_scale', payload=np.array([guidance_scale])).dict(),
+    #         NumpyCodec.encode_input(name='max_new_tokens', payload=np.array([max_new_tokens])).dict()
+    #     ]
+    # }
+    # result = requests.post(endpoint, json=input_request).json()
+    # audio_array = np.array(result['outputs'][0]['data'])
+
+    # file_name = os.path.join('./music', f"{faker.user_name()}.mp3")
+    # with AudioFile(file_name, "w", samplerate=sample_rate, num_channels=1) as f:
+    #     f.write(audio_array)
+    # return sample_rate, audio_array
+    pass
+
+
+def audio_effect():
+    # endpoint = "http://localhost:7050/v2/models/novice_dj/infer"
+    # files =  glob("./music/*.mp3")
+    # latest_file = max(files, key=os.path.getctime)
+    # with AudioFile(latest_file, "r") as f:
+    #     waveform = f.read(f.frames)
+    #     sample_rate = f.samplerate
+    # input_request = {
+    #     'inputs': [
+    #         NumpyCodec.encode_input(name='song', payload=waveform).dict(),
+    #         NumpyCodec.encode_input(name='sample_rate', payload=np.array([sample_rate])).dict()
+    #     ]
+    # }
+    # result = requests.post(endpoint, json=input_request).json()
+    # audio_array = np.array(result['outputs'][0]['data'])
+    # return gr.make_waveform((sample_rate, audio_array), bg_image="bg.png")
+    pass
 
 
 with gr.Blocks(theme="gstaff/xkcd") as demo:
